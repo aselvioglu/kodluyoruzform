5ad06c505c8255dbf0f46e3a10615997b9302bc7
Aydin Selvioglu <aydinselvioglu01@gmail.com>
2023-08-28 22:35:40 +0300

    Formla listeye veri ekleme egzersizi

---
+++ App.js
@@ -0,0 +1,33 @@
+let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener("submit", formSubmit);
+const alertDOM=document.querySelector("#alert");
+
+function formSubmit(event) {
+    event.preventDefault();
+    console.log("islem gerçekleştirildi");
+    let scoreDOM = document.querySelector("#score");
+    let userNameDOM = document.querySelector("#userName");
+    console.log(userName.value);
+    console.log(scoreDOM.value);
+    localStorage.setItem("userName", userNameDOM.value);
+    localStorage.setItem("score", scoreDOM.value);
+    if (userNameDOM.value, scoreDOM.value) {
+
+        addItem(userNameDOM.value, scoreDOM.value);
+    }
+    else {
+
+        alertDOM.innerHTML="hatalı bilgi girdiniz"
+    }
+
+}
+
+let userListDOM = document.querySelector("#userList");
+
+
+const addItem = (userName, score) => {
+    let liDOM = document.createElement("li");
+    liDOM.innerHTML = `${userName} ${score}`;
+    userListDOM.appendChild(liDOM)
+
+}
